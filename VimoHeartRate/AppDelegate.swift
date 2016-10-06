99da6922f7bbd8869917b7f7638c8929bdaf9ed8
Brian VanderZanden <turbobrian@users.noreply.github.com>
2016-10-05 18:29:01 -0700

    Fixes warning from xcode 8.
    Conversation about change: http://stackoverflow.com/questions/38972440/application-didfinishlaunchingwithoptions-nearly-matches-optional-requireme#38973284

--- VimoHeartRate/AppDelegate.swift
+++ VimoHeartRate/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let healthStore = HKHealthStore()
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
