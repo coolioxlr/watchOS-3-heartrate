519c65fbafd421b5e9c86f45c622812172d0052b
Dmitrii Ch <dimkoy01@gmail.com>
2019-10-12 17:42:44 +0200

    Bump swift version to 5.0

--- VimoHeartRate/AppDelegate.swift
+++ VimoHeartRate/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let healthStore = HKHealthStore()
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    private func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
